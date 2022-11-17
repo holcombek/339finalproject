ec353e5da851c60e4d5d45ec8d19f1baa84ba0f9
holcombek <73794810+holcombek@users.noreply.github.com>
2022-11-17 15:58:24 -0500

    Add files via upload

---
+++ js/work.js
@@ -0,0 +1,5 @@
+document.querySelector('.nav_item').addEventListener("mouseover",function(){
+    navEl = document.querySelector('.nav_item')
+    //navEl.style.backgroundColor = 'lightblue'
+    navEl.className = 'active';
+})
